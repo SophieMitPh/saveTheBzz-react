6e248c7726ee93192e5496666ec6920d3f5d05a6
sophieMitPh <sophie.pr@gmx.de>
2020-06-21 16:52:04 +0200

    fixed indents

--- client/src/productsOverview.js
+++ client/src/productsOverview.js
@@ -21,42 +21,32 @@ function ProductsOverview() {
 
 	const renderProduct = product => {
 		return (
-			<div class="page">
-
-			<div class="product-grid">
-			<li key={product._id}>
-			<div>
-
-			<div class="imgBorder">
-			<a href={"/products/product/" + product.name}><img class="product-image" src="Shirt.png" /></a>
-			<div class="belowpic">
-			<h2 id="h2-item-name">{product.name}</h2>
-			<a href="/wishlist">
-			<img class="wishlistIcon" src="wishList.png"/>
-			</a>
-			</div>
-			</div>
-			</div>
+			<li key={product.name}>
+				<div className="imgBorder">
+					<a href={"/products/product/" + product.name}>
+						<img className="product-image" src={product.name + ".png"}/>
+						</a>
+					<div className="belowpic">
+						<h2 id="h2-item-name">{product.name}</h2>
+						<a href="/wishlist"><img className="wishlistIcon" src="wishList.png"/></a>
+					</div>
+				</div>
 			</li>
-			</div>
-			</div>
-	);
+		)
 	};
 
 	return (
-		<div className="App">
-		<div class="productTitle">
-		<h1>Products</h1>
+		<div className="page">
+			<h2 className="productTitle">
+				Products
+			</h2>
+		{(products && products.length > 0) ? (
+			products.map(product => renderProduct(product))
+		) : (
+			<p>No products found</p>
+		)}
 		</div>
-		<ul className="list">
-	{(products && products.length > 0) ? (
-		products.map(product => renderProduct(product))
-	) : (
-		<p>No products found</p>
-)}
-</ul>
-	</div>
-);
+	);
 }
 
 export default ProductsOverview;
