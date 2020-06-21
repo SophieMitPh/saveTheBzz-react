d416c348ec363f642e8c5de37e4e04a3b7e0348d
SophieMitPh <sophie.pr@gmx.de>
2020-06-21 11:20:20 +0200

    setup product model and API

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+require('./models/Product');
+
 const app = express();
 
 const MONGODB_URI =  process.env.MONGODB_URI || ((process.env.NODE_ENV === 'test') ? 'mongodb://localhost:27017/save-the-bzz_test_db' : 'mongodb://localhost:27017/save-the-bzz');
@@ -11,6 +13,16 @@ mongoose.Promise = global.Promise;
 mongoose.connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  });
 
 app.use(bodyParser.json());
+
+require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req,res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
