966190f36f652d0eea6b9809a1e19c467b0b60dc
sophieMitPh <sophie.pr@gmx.de>
2020-07-12 14:01:08 +0200

    added redirect to addProduct

--- client/src/components/product/AddProduct.js
+++ client/src/components/product/AddProduct.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import '../../css/AddProduct.css';
 import productService from './../../services/productService';
 
-const AddProduct = () => {
+const AddProduct = (props) => {
 
 	const initialProductState = {
 		id: null,
@@ -18,7 +18,8 @@ const AddProduct = () => {
 		setProduct({ ...product, [name]: value })
 	};
 
-	const saveProduct = () => {
+	const saveProduct = (e) => {
+		e.preventDefault();
 		var data = {
 			name: product.name,
 			description: product.description,
@@ -38,17 +39,18 @@ const AddProduct = () => {
 			.catch(e => {
 				console.log(e);
 			});
+		props.history.push('/products/index');
 	};
 
 	return (
 		<div>
 		<h2>Add a new product to your webshop!</h2>
 			<div className="signUpRectangle">
-				<form className="form-container">
+				<form onSubmit={(e) => saveProduct(e)} className="form-container">
 					<input id="nameValue" type="text" value={product.name} onChange={handleInputChange} placeholder="Product Name" name="name" required/>
 						<textarea type="text" value={product.description} onChange={handleInputChange} placeholder="Description" name="description" required/>
 						<input value={product.price} onChange={handleInputChange} placeholder="Price" name="price" required />
-						<button type="submit" onClick={saveProduct} id="signup-button">ADD THIS PRODUCT</button>
+						<button type="submit" id="signup-button">ADD THIS PRODUCT</button>
 				</form>
 			</div>
 		</div>
